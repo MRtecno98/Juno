35d2620f9a518d5876810ec17eb4bd3226d1879f
MRtecno98 <mr.tecno98@gmail.com>
2024-04-26 13:18:10 +0200

    fix lombok configuration

--- src/main/java/dev/mrtecno/juno/util/Optionals.java
+++ src/main/java/dev/mrtecno/juno/util/Optionals.java
@@ -1,13 +1,14 @@
 package dev.mrtecno.juno.util;
 
-import lombok.experimental.UtilityClass;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 import java.util.function.Consumer;
 import java.util.function.UnaryOperator;
 
-@UtilityClass
-public class Optionals {
-	public <T> UnaryOperator<T> peek(Consumer<T> c) {
+@NoArgsConstructor(access=AccessLevel.PRIVATE)
+public final class Optionals {
+	public static <T> UnaryOperator<T> peek(Consumer<T> c) {
 		return x -> {
 			c.accept(x);
 			return x;
